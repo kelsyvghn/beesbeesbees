8465947d631812c9020044059dc3d7f6034f667b
Kelsy Vaughn <angelstate@gmail.com>
2020-02-10 10:08:55 -0800

    Grub intial data

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,17 @@
-var Grub = function() {
+var Grub = function() {  // Create a Grub class, in pseudoclassical style, with:
+
+  // an age property that is set to 0
+  this.age = 0;
+  // a color property that is set to pink
+  this.color = 'pink';
+ //a food property that is set to jelly
+  this.food = 'jelly';
+
 };
+
+
+// an eat method
+Grub.prototype.eat = function (){
+
+}
 
